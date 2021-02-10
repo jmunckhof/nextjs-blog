39e357d2de7522211c4880b0bcd09e6354a610f8
Jesper van den Munckhof <jesper@munckhof.name>
2021-02-10 15:49:37 +0100

    first comit

---
+++ pages/admin/[slug].js
@@ -0,0 +1,10 @@
+import MetaTags from "../../components/MetaTags"
+
+export default function AdminPostEdit({ }) {
+  return (
+    <main>
+      <MetaTags title="Admin Page" />
+        <h1>Edit Post</h1>
+    </main>
+  )
+}
